24611a096f57387b78df91b6d8f572e6cbc8bd31
武晓慧 <wxh199311@outlook.com>
2022-01-04 19:57:10 +0800

    v2.3

--- src/utils/notify.js
+++ src/utils/notify.js
@@ -1,7 +1,72 @@
 import { Notify } from 'vant'
+
 import { curry } from 'lodash'
 
-const createNotify = curry((type, message) => Notify({ type, message }), 2)
-export const createDangerNotify = createNotify('danger')
-export const createWarningNotify = createNotify('warning')
-export const createSuccessNotify = createNotify('success')
+const createNotify = curry((type, message) => {
+  return Notify({
+    type,
+    message
+  })
+})
+
+const warningShow = createNotify('warning')
+warningShow('提示消息')
+
+// createNotify('danger')('提示信息')
+
+// const warningShow = createNotify('warning')
+// createNotify('danger')('提示')
+// const dangerShow = createNotify('danger')
+//
+// dangerShow('提示')
+
+// warningShow('提示')
+
+// 函数柯里化
+// 闭包
+//
+//
+// Notify({
+//   type: 'danger',
+//   message: '提示'
+// })
+
+// function showDanger (message) {
+//   Notify({
+//     type: 'danger',
+//     message
+//   })
+// }
+//
+// function showWarning (message) {
+//   Notify({
+//     type: 'warning',
+//     message
+//   })
+// }
+// function showSuccess (message) {
+//   Notify({
+//     type: 'success',
+//     message
+//   })
+// }
+
+// const createNotify = curry((type, message) => Notify({ type, message }), 2)
+// export const createDangerNotify = createNotify('danger')
+// export const createWarningNotify = createNotify('warning')
+// export const createSuccessNotify = createNotify('success')
+
+function match (reg, str) {
+  return str.match(reg)
+}
+
+const regMath = curry(match)
+
+const matchNumber = regMath(/\d/)
+regMath()
+
+console.log(matchNumber(1))
+
+console.log(match(/\d/, 1))
+console.log(match(/\d/, 2))
+console.log(match(/\d/, 3))
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,9 +1,24 @@
 import axios from 'axios'
 import store from '@/store'
 
+import jsonBig from 'json-bigint'
+
+const jsonObj = '{"art_id": 9007199254740995, "name": "黑马"}'
+
+console.log(jsonBig.parse(jsonObj).art_id.toString())
+
 const request = axios.create({
-  baseURL: 'http://weldon.net.cn:8000'
+  baseURL: 'http://weldon.net.cn:8000',
+  transformResponse: [function (data) {
+    try {
+      return jsonBig.parse(data)
+    } catch (e) {
+      return data
+    }
+  }]
 })
+
+// console.log(jsonObj)
 
 // 请求拦截器
 request.interceptors.request.use(function (config) {
